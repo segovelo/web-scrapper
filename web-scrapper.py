eb97dc3403659b490bda95ab08cdf89091200fed
segovelo <gauchosinpingo@gmail.com>
2022-10-16 22:58:06 +0100

    Changes to fix driver

--- web-scrapper.py
+++ web-scrapper.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-website = "https://www.thesun.co.uk/sport/football/"
-path = "C:/Users/sebas/chromedriver_win32/chromedriver.exe"
 
-#service = Service(executable_path=r"C:/Users/sebas/chromedriver_win32/chromedriver.exe")
-driver = webdriver.Chrome(executable_path=r"C:/Users/sebas/chromedriver_win32/chromedriver.exe")
+website = "https://www.thesun.co.uk/sport/football/"
+path = "/Users/Sebastian/Web-Dev/chromedriver_win32/chromedriver.exe"
+
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
 driver.get(website)
