9c7a63cc9d06ad9371da6caf1c46c8c92bfbf41e
segovelo <gauchosinpingo@gmail.com>
2022-10-23 00:56:28 +0100

    Changes to save file with date-time

--- web-scrapper.py
+++ web-scrapper.py
@@ -2,17 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
+from datetime import datetime
 from dotenv import load_dotenv
 import os
+import sys
 
+print("Starting the program...")
 load_dotenv()
 path = os.getenv('CHROME_DRIVER_PATH')
 website = "https://www.thesun.co.uk/sport/football/"
 #path = "C:/<YOUR_PATH>/chromedriver_win32/chromedriver.exe"
 
+app_path = os.path.dirname(sys.executable)
+now = datetime.now()
+date_time = now.strftime("%Y-%m-%dT%H-%M-%S") 
+ 
 # headless-mode
 options = Options()
 options.headless = True
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
 
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service, options=options)
@@ -29,6 +38,12 @@ for container in containers:
 
 my_dict = {'title': titles, 'subtitle': subtitles, 'link': links}
 df_headlines = pd.DataFrame(my_dict)
-df_headlines.to_csv('headlines-headless.csv')
+
+file_name = f'headlines-{date_time}.csv'
+full_path = os.path.join(app_path, file_name)
+
+df_headlines.to_csv(file_name)
 
 driver.quit()
+print("Program Finished successfully")
+
